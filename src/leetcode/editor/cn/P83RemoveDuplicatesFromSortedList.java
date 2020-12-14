6581ef57cdbe299615de301ebb1802d255514d63
zhuo <790489690@qq.com>
2020-12-14 09:14:46 +0800

    [master] 动态规划

---
+++ src/leetcode/editor/cn/P83RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,52 @@
+package leetcode.editor.cn;
+//给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。 
+//
+// 示例 1: 
+//
+// 输入: 1->1->2
+//输出: 1->2
+// 
+//
+// 示例 2: 
+//
+// 输入: 1->1->2->3->3
+//输出: 1->2->3 
+// Related Topics 链表 
+// 👍 436 👎 0
+
+import leetcode.editor.cn.linked.data.ListNode;
+
+public class P83RemoveDuplicatesFromSortedList {
+    public static void main(String[] args) {
+        Solution solution = new P83RemoveDuplicatesFromSortedList().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public ListNode deleteDuplicates(ListNode head) {
+            if (head == null) {
+                return null;
+            }
+            ListNode curr = head;
+            while (curr != null && curr.next != null) {
+                if (curr.val == curr.next.val) {
+                    curr.next = curr.next.next;
+                } else {
+                    curr = curr.next;
+                }
+            }
+            return  head;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
